858b5bcef3d33308db8b78f46ea79e284ce75bf0
Dristi <dristipatel@gmail.com>
2026-01-10 08:34:35 -0800

    fix API call headers and formatting

--- functions/src/types/cat-info.ts
+++ functions/src/types/cat-info.ts
@@ -1,9 +1,12 @@
 interface ShelterluvCat {
-  id: string;
-  name: string;
-  status: string;
-  photo_url?: string;
-  intake_date?: string;
+  ID: string;
+  Name: string;
+  Sex?: string;
+  Color?: string;
+  Pattern?: string;
+  CoverPhoto?: string;
+  LastIntakeUnixTime?: number;
+  InFoster: boolean;
 }
 
 export { ShelterluvCat };
